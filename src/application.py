67f287f02dd8d5194b692dbb5abf3cb45d4203e3
Raphael Golubev <raphael.golubev@gmail.com>
2024-09-26 00:24:46 +0300

    ansi colors for easy reading

--- src/application.py
+++ src/application.py
@@ -1,21 +1,32 @@
 from fastapi import FastAPI
 
+from src.utils.ansi_colors import Colors, colorize
 
-app = FastAPI()
+
+def _colored_print(shutdown: bool = False, startup: bool = False):
+    msg = ''
+    if startup:
+        msg = colorize("STARTUP", Colors.LIGHT_GREEN)
+    else:
+        msg = colorize("SHUTDOWN", Colors.LIGHT_RED)
+    print(f" -- Application {msg} event has been triggered -- ")
 
 
 async def startup():
-    print("Application STARTUP event has been triggered")
+    _colored_print(startup=True)
 
 
 async def shutdown():
-    print("Application SHUTDOWN event has been triggered")
+    _colored_print(shutdown=True)
 
 
 async def lifespan(app: FastAPI):
     await startup()
     yield
     await shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
 
 
 @app.get("/ping")
