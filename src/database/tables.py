d11085d102958bacdea13b8805199d93830cfe6c
Raphael Golubev <raphael.golubev@gmail.com>
2024-09-29 01:36:51 +0300

    added drop_all method

--- src/database/tables.py
+++ src/database/tables.py
@@ -1,5 +1,5 @@
 from sqlalchemy.dialects import postgresql
-from sqlalchemy.schema import CreateTable
+from sqlalchemy.schema import CreateTable, DropTable
 
 from src.database import database
 from src.api.models import Base
@@ -12,6 +12,16 @@ async def create_all():
     for table in Base.metadata.tables.values():
         print(ANSI(f"Creating table: {table.name}").purple.end)
         schema = CreateTable(table, if_not_exists=True)
+        query = str(schema.compile(dialect=postgresql.dialect()))
+
+        await database.execute(query)
+
+
+async def drop_all():
+    print(ANSI("---  Dropping tables ---").purple.bg.end)
+    for table in Base.metadata.tables.values():
+        print(ANSI(f"Dropping table: {table.name}").purple.end)
+        schema = DropTable(table, if_exists=True)
         query = str(schema.compile(dialect=postgresql.dialect()))
 
         await database.execute(query)
