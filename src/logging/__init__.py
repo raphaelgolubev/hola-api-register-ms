baa43d327fb7c0db83baa5bc15bf587ad2ee8697
Raphael Golubev <raphael.golubev@gmail.com>
2024-09-30 20:51:48 +0300

    improved logging

--- src/logging/__init__.py
+++ src/logging/__init__.py
@@ -1,15 +1,26 @@
 import sys
-from loguru import logger
 
+from typing import Any
+from loguru import logger as loguru_logger
+
+
+loguru_logger.remove()
 
 RECORD_FORMAT = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level.icon} <level>{level: <8}</level> | {file.path} | {module}:{line} | {function} | <level>{message}</level>"
 ROTATION = "10 MB"
 RETENTION = "7 days"
 COMPRESSION = "zip"
 
-logger.remove()
-logger.add(sys.stderr, level="DEBUG", format=RECORD_FORMAT)
+# functions
+def add(path: str, level: str):
+    loguru_logger.add(
+        sink=path,
+        level=level.upper(),
+        filter=lambda record: record["level"].name == level.upper(),
+        **params,
+    )
 
+# Params
 params = {
     "rotation": ROTATION,
     "retention": RETENTION,
@@ -21,23 +32,46 @@ params = {
     "diagnose": False,
 }
 
-logger.add(
-    sink="logs/info.json",
-    level="INFO",
-    filter=lambda record: record["level"].name == "INFO",
-    **params,
-)
+# Custom levels
+loguru_logger.level("REQUEST", no=15, color="<blue>", icon="📩")
 
-logger.add(
-    sink="logs/info.json",
-    level="SUCCESS",
-    filter=lambda record: record["level"].name == "SUCCESS",
-    **params,
-)
+# Standard output sinks
+loguru_logger.add(sys.stderr, level="DEBUG", format=RECORD_FORMAT)
 
-logger.add(
-    sink="logs/error.json",
-    level="ERROR",
-    filter=lambda record: record["level"].name == "ERROR",
-    **params,
-)
+# File sinks
+add("logs/request.json", "REQUEST")
+add("logs/info.json", "INFO")
+add("logs/info.json", "SUCCESS")
+add("logs/error.json", "ERROR")
+
+
+class Logger:
+    def __init__(self):
+        pass
+
+    def trace(self, *args: Any, **kwargs: Any):
+        loguru_logger.trace(*args, **kwargs)
+
+    def debug(self, *args: Any, **kwargs: Any):
+        loguru_logger.debug(*args, **kwargs)
+
+    def info(self, *args: Any, **kwargs: Any):
+        loguru_logger.info(*args, **kwargs)
+
+    def success(self, *args: Any, **kwargs: Any):
+        loguru_logger.success(*args, **kwargs)
+
+    def warning(self, *args: Any, **kwargs: Any):
+        loguru_logger.warning(*args, **kwargs)
+
+    def error(self, *args: Any, **kwargs: Any):
+        loguru_logger.error(*args, **kwargs)
+
+    def critical(self, *args: Any, **kwargs: Any):
+        loguru_logger.critical(*args, **kwargs)
+    
+    def request(self, *args: Any, **kwargs: Any):
+        loguru_logger.log("REQUEST", *args, **kwargs)
+
+
+logger = Logger()
