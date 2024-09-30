4dd85175208f3d68a0ac2c5d22f8c01a86375b5e
Raphael Golubev <raphael.golubev@gmail.com>
2024-09-30 20:26:11 +0300

    fix logging

--- src/logging/__init__.py
+++ src/logging/__init__.py
@@ -1,73 +1,43 @@
 import sys
 from loguru import logger
 
-from src.utils.ansi_colors import ANSI
-
 
 RECORD_FORMAT = "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level.icon} <level>{level: <8}</level> | {file.path} | {module}:{line} | {function} | <level>{message}</level>"
 ROTATION = "10 MB"
 RETENTION = "7 days"
 COMPRESSION = "zip"
 
-
-def info_only(record):
-    return record["level"].name == "INFO"
-
-def success_only(record):
-    return record["level"].name == "SUCCESS"
-
-def error_only(record):
-    return record["level"].name == "ERROR"
-
-
 logger.remove()
-
 logger.add(sys.stderr, level="DEBUG", format=RECORD_FORMAT)
-logger.add(sys.stdout, level="INFO", format=RECORD_FORMAT)
-logger.add(sys.stdout, level="SUCCESS", format=RECORD_FORMAT)
-logger.add(sys.stdout, level="ERROR", format=RECORD_FORMAT)
+
+params = {
+    "rotation": ROTATION,
+    "retention": RETENTION,
+    "compression": COMPRESSION,
+    "format": RECORD_FORMAT,
+    "colorize": None,
+    "backtrace": True,
+    "serialize": True,
+    "diagnose": False,
+}
 
 logger.add(
-    "logs/info.json",
+    sink="logs/info.json",
     level="INFO",
-
-    rotation=ROTATION,
-    retention=RETENTION,
-    compression=COMPRESSION,
-    format=RECORD_FORMAT,
-    filter=info_only,
-
-    colorize=True,
-    backtrace=True,
-    serialize=True,
+    filter=lambda record: record["level"].name == "INFO",
+    **params,
 )
 
 logger.add(
-    "logs/info.json",
+    sink="logs/info.json",
     level="SUCCESS",
-
-    rotation=ROTATION,
-    retention=RETENTION,
-    compression=COMPRESSION,
-    format=RECORD_FORMAT,
-    filter=success_only,
-
-    colorize=True,
-    backtrace=True,
-    serialize=True,
+    filter=lambda record: record["level"].name == "SUCCESS",
+    **params,
 )
 
 logger.add(
-    "logs/error.json",
+    sink="logs/error.json",
     level="ERROR",
-
-    rotation=ROTATION,
-    retention=RETENTION,
-    compression=COMPRESSION,
-    format=RECORD_FORMAT,
-    filter=error_only,
-
-    colorize=True,
-    backtrace=True,
-    serialize=True,
+    filter=lambda record: record["level"].name == "ERROR",
+    **params,
 )
