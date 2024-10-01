a99bcadacc97eea4a2b3ecc5d7de12c0980f8ef7
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-01 21:35:52 +0300

    fix logger name, added new exception

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException
 
 from src.api.schemas import RegisterIn, RegisterOut
-from src.api.exceptions import EmailAlreadyExistsError
-from src.logging import loguru_logger
+from src.api.exceptions import EmailAlreadyExistsError, PhoneAlreadyExistsError
+from src.logging import logger
 
 from src.api.dependencies import RegisterServiceDep
 
@@ -10,23 +10,23 @@ from src.api.dependencies import RegisterServiceDep
 router = APIRouter()
 
 
-@loguru_logger.catch
+@logger.catch
 @router.post(
     "/create_user",
     name="Создание нового пользователя",
     response_model=RegisterOut,
 )
-async def create_user(
-    register_data: RegisterIn, 
-    service: RegisterServiceDep,
-):
+async def create_user(input: RegisterIn, service: RegisterServiceDep):
     """
     Создание нового пользователя в БД.
     Возвращает id и тип нового пользователя.
     """
     try:
-        db_model = await service.add_user(schema=register_data)
+        db_model = await service.add_user(schema=input)
         return RegisterOut.model_validate(db_model, from_attributes=True)
 
     except EmailAlreadyExistsError:
-        raise HTTPException(status_code=400, detail="Пользователь с таким email уже существует")
+        raise HTTPException(status_code=400, detail="Пользователь с таким адресом электронной почты уже существует")
+    
+    except PhoneAlreadyExistsError:
+        raise HTTPException(status_code=400, detail="Пользователь с таким номера телефона уже существует")
