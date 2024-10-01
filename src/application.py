9519831ead09972d333a053d49f18d351c7a08ba
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-01 20:20:10 +0300

    lifecycle methods update

--- src/application.py
+++ src/application.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-from src.database import database
 from src.database.tables import create_all, drop_all
 from src.utils.ansi_colors import ANSI
 from src.routing import main_router
@@ -10,23 +9,21 @@ from src.logging import logger
 async def startup():
     logger.info("Starting up...")
     try:
-        logger.info(f"Connecting to database: {database.url}")
-        await database.connect()
         await create_all()
     except Exception as e:
-        logger.error(f"Error connecting to database: {str(e)}")
-    logger.success(f"Connected to database, tables created")
+        logger.error(f"Error during tables creating: {str(e)}")
+    else:
+        logger.success(f"Connected to database, tables created")
 
 
 async def shutdown():
     logger.info("Shutting down...")
     try:
         await drop_all()
-        logger.info(f"Disconnecting from database")
-        await database.disconnect()
     except Exception as e:
-        logger.error(f"Error disconnecting from database: {str(e)}")
-    logger.success("Disconnected from database")
+        logger.error(f"Error during dropping tables: {str(e)}")
+    else:
+        logger.success("Disconnected from database")
 
 
 async def lifespan(app: FastAPI):
