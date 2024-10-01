6fcd579ed2647d07bdb3837635ea603d949b9630
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-01 20:20:43 +0300

    update repository code to full ORM based way

--- src/repo/__init__.py
+++ src/repo/__init__.py
@@ -1,74 +1,118 @@
-from sqlalchemy import Table
+from sqlalchemy import select, delete
+from sqlalchemy.ext.asyncio import async_sessionmaker
 from uuid import UUID
 
 from src.interfaces import IRepository, ModelType, SchemaType
-from src.database import database
+from src.database import engine
 from src.utils.ansi_colors import ANSI
 from src.logging import logger
+
+import src.repo.exceptions as exc
 
 
 class SQARepository(IRepository):
     def __init__(self, model: ModelType):
         self.model = model
-        self.table = Table(
-            self.model.__tablename__, 
-            self.model.metadata, 
-            autoload=True
+        self.session = async_sessionmaker(
+            bind=engine, 
+            autoflush=False, 
+            expire_on_commit=False, 
+            autocommit=False
         )
 
-    async def _retrieve(self, id: str):
-        """ 
-        Выполняет запрос `SELECT * FROM <table> WHERE id = :id` для текущей таблицы.
-        Потому что метод `database.execute` не умеет 
-        работать с `.returning()` модуля SQLAlchemy,
-        а именно возвращать более чем один столбец.
-        """
-        select = f"SELECT * FROM {self.model.__tablename__} WHERE id = :id"
-        record = await database.fetch_one(query=select, values={"id": id})
+    async def create(self, schema: SchemaType) -> ModelType | None:
+        db_model = self.model(**schema.model_dump())
 
-        return record
+        async with self.session() as session:
+            try:
+                session.add(db_model)
+                await session.commit()
+                await session.refresh(db_model)
 
-    async def create(self, schema: SchemaType):
-        query = self.table.insert()
-        query = query.values(**schema.model_dump())
-        query = query.returning(self.table.c.id)
+                return db_model
+            except exc.IntegrityError:
+                raise exc.IntegrityConflictError(
+                    f"{self.model.__tablename__} conflicts with existing data."
+                )
+            except Exception as e:
+                raise exc.RepositoryError(f"Неизвестная ошибка: {e}") from e
 
-        logger.debug(ANSI(f"{query};\nUsing values: {query.compile().params}").purple.end.replace("\n", ""))
+    async def create_many(self, schemas: list[SchemaType]) -> list[ModelType]:
+        db_models = [self.model(**schema.model_dump()) for schema in schemas]
 
-        id = await database.execute(query=query)
-        record = await self._retrieve(id=id)
+        async with self.session() as session:
+            try:
+                session.add_all(db_models)
+                await session.commit()
 
-        if record:
-            return self.model(**record)
+                for model in db_models:
+                    await session.refresh(model)
 
-        return None
+                return db_models
+            except exc.IntegrityError:
+                raise exc.IntegrityConflictError(
+                    f"{self.model.__tablename__} conflicts with existing data."
+                )
+            except Exception as e:
+                raise exc.RepositoryError(f"Неизвестная ошибка: {e}") from e
 
-    async def create_many(self, schemas: list[SchemaType]):
-        table = Table(self.model.__tablename__, self.model.metadata, autoload=True)
-        query = table.insert()
 
-        logger.debug(ANSI(f"Making INSERT: {query}; Using many values").purple.end.replace("\n", ""))
+    async def get_one_by(self, value: str, column: str, with_for_update: bool = False) -> ModelType | None:
+        try:
+            query = select(self.model).where(getattr(self.model, column) == value)
+        except AttributeError:
+            raise exc.RepositoryError(f"Столбец {column} не найде в таблице {self.model.__tablename__}")
 
-        values = [schema.model_dump() for schema in schemas]
-        await database.execute_many(query=query, values=values)
+        if with_for_update:
+            query = query.with_for_update()
 
-    async def get_one_by(self, value: str, column: str):
-        query = self.table.select()
-        query = query.where(getattr(self.table.c, column) == value)
+        async with self.session() as session:
+            results = await session.execute(query)
+            return results.unique().scalar_one_or_none()
 
-        logger.debug(ANSI(f"Making SELECT: {query}; Where {column} = {value}").purple.end.replace("\n", ""))
+    async def get_many_by(self, values: list[str | UUID], column: str, with_for_update: bool = False):
+        query = select(self.model)
+        if values:
+            try:
+                query = query.where(getattr(self.model, column).in_(values))
+            except AttributeError:
+                raise exc.RepositoryError(f"Столбец {column} не найде в таблице {self.model.__tablename__}")
 
-        record = await database.fetch_one(query=query)
+        if with_for_update:
+            query = query.with_for_update()
+
+        async with self.session() as session:
+            rows = await session.execute(query) 
+            return rows.unique().scalars().all()
+
+    async def update_one_by(self, data: SchemaType, value: str, column: str) -> ModelType:
+        db_model = await self.get_one_by(value=value, column=column, with_for_update=True)
+
+        if not db_model:
+            raise exc.NotFoundError(f"{self.model.__tablename__} {column}={value} не найден")
+
+        schema_values = data.model_dump(exclude_unset=True)
+        for k, v in schema_values.items():
+            setattr(db_model, k, v)
+
+        async with self.session() as session:
+            try:
+                await session.commit()
+                await session.refresh(db_model)
+                return db_model
+            except exc.IntegrityError:
+                raise exc.IntegrityConflictError(
+                    f"{self.model.__tablename__} {column}={value} conflicts with existing data."
+                )
+
+    async def remove_by(self, value: str, column: str) -> int:
+        try:
+            query = delete(self.model).where(getattr(self.model, column) == value)
+        except AttributeError:
+            raise exc.RepositoryError(f"Столбец {column} не найден в таблице {self.model.__tablename__}")
         
-        if record:
-            return self.model(**record)
+        async with self.session() as session:
+            rows = await session.execute(query)
+            await session.commit()
 
-        return None
-
-    async def get_many_by_id(self, ids: list[str | UUID], column: str = "id"):
-        table = Table(self.model.__tablename__, self.model.metadata, autoload=True)
-        query = table.select().where(getattr(table.c, column).in_(ids))
-
-        logger.debug(ANSI(f"Making SELECT: {query}; Using ids: {ids}").purple.end.replace("\n", ""))
-
-        await database.fetch_many(query=query)
+            return rows.rowcount
