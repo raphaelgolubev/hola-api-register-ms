db6ca5f04fc1a7d9e9bd8df537a6c9852f284667
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-01 20:22:03 +0300

    api handler update

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,11 +1,10 @@
-from fastapi import APIRouter, Request, HTTPException
-
-from src.database.tables import User
+from fastapi import APIRouter, HTTPException
 
 from src.api.schemas import RegisterIn, RegisterOut
-from src.api.service import get_service
 from src.api.exceptions import EmailAlreadyExistsError
 from src.logging import loguru_logger
+
+from src.api.dependencies import RegisterServiceDep
 
 
 router = APIRouter()
@@ -13,17 +12,18 @@ router = APIRouter()
 
 @loguru_logger.catch
 @router.post(
-    "/sign_up",
+    "/create_user",
     name="Создание нового пользователя",
     response_model=RegisterOut,
 )
-async def sign_up(register_data: RegisterIn, request: Request):
+async def create_user(
+    register_data: RegisterIn, 
+    service: RegisterServiceDep,
+):
     """
-    Регистрация нового пользователя.
+    Создание нового пользователя в БД.
     Возвращает id и тип нового пользователя.
     """
-    service = get_service(model=User)
-
     try:
         db_model = await service.add_user(schema=register_data)
         return RegisterOut.model_validate(db_model, from_attributes=True)
