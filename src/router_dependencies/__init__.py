def171969395d50651a8215d554d81ce9eddcc0b
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-01 21:37:12 +0300

    fix logger calls

--- src/router_dependencies/__init__.py
+++ src/router_dependencies/__init__.py
@@ -4,11 +4,11 @@ from src.logging import logger
 
 
 async def log_request(request: Request):
-    logger.request(f"Request: {request.method} {request.url}")
-    logger.request(f"Client: {request.client.host}:{request.client.port}")
-    logger.request("Params:")
+    logger.log("REQUEST", f"Request: {request.method} {request.url}")
+    logger.log("REQUEST", f"Client: {request.client.host}:{request.client.port}")
+    logger.log("REQUEST", "Params:")
     for name, value in request.path_params.items():
-        logger.request(f"\t{name}: {value}")
-    logger.request("Headers:")
+        logger.log("REQUEST", f"\t{name}: {value}")
+    logger.log("REQUEST", "Headers:")
     for name, value in request.headers.items():
-        logger.request(f"\t{name}: {value}")
+        logger.log("REQUEST", f"\t{name}: {value}")
