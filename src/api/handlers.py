48cbd120901659b52d29e3eb8f59317ba31820d1
Raphael Golubev <raphael.golubev@gmail.com>
2024-10-03 18:06:36 +0300

    update handlers

--- src/api/handlers.py
+++ src/api/handlers.py
@@ -1,3 +1,4 @@
+import traceback
 from fastapi import APIRouter, HTTPException
 
 from src.api.schemas import RegisterIn, RegisterOut
@@ -19,14 +20,39 @@ router = APIRouter()
 async def create_user(input: RegisterIn, service: RegisterServiceDep):
     """
     Создание нового пользователя в БД.
+    В случае удачного создания пользователя отправляет код на почту.
     Возвращает id и тип нового пользователя.
     """
     try:
         db_model = await service.add_user(schema=input)
-        return RegisterOut.model_validate(db_model, from_attributes=True)
+        service.send_email_code(db_model.email)
 
     except EmailAlreadyExistsError:
         raise HTTPException(status_code=400, detail="Пользователь с таким адресом электронной почты уже существует")
-    
+
     except PhoneAlreadyExistsError:
         raise HTTPException(status_code=400, detail="Пользователь с таким номера телефона уже существует")
+
+    except Exception as e:
+        logger.error(f"{e}, {traceback.format_exc()}")
+        raise HTTPException(status_code=500, detail="Неизвестная ошибка")
+
+    else:
+        
+        return RegisterOut.model_validate(db_model, from_attributes=True)
+
+
+@router.post(
+    "/send_email_code",
+    name="Отправка кода на адрес электронной почты",
+)
+async def send_email_code(user_id: str):
+    pass
+
+
+@router.post(
+    "/verify_email_code",
+    name="Подтверждение отправленного кода",
+)
+async def verify_email_code(user_id: str):
+    pass
